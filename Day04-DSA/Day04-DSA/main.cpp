1582e9ee4e7e0f0d846b521f9d1d44439f89a651
Harsh M. Yadav <98019495+InsanelyHarsh@users.noreply.github.com>
2022-06-02 18:53:02 +0530

    Day 04 is ending Revised Linked List and solved some more question on HackerRank

--- Day04-DSA/Day04-DSA/main.cpp
+++ Day04-DSA/Day04-DSA/main.cpp
@@ -64,9 +64,92 @@ bool checkPalindrome(char name[]){
     return 0;
 } //basic implementation
 
+int camelcase(std::string s) {
+    int count = 1;
+    for(int i=0;i<s.size();i++){
+        if(65<=s[i] && s[i]<=90){
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+
+
+//MARK: Linked List
+struct Node{
+    int data;
+    Node *next;
+};
+
+Node* head = NULL; //Head Pointer, initially pointing to NULL as Linked List is empty.
+void insertNode(int x){
+    
+    Node * temp = new Node; //Creating new node.
+    temp->next = NULL; // New Node is Created pointing to NULL.
+    temp->data = x; //Inserting Data in Node.
+    
+    temp->next = head; //new node will point to Head,which was pointing to previous node....
+    head = temp; //now head will point to
+}
+
+void printLinkedList(){
+    Node *tmp = head;
+    while(tmp !=NULL){
+        std::cout<<tmp->data<<" ";
+        tmp = tmp->next;
+    }
+    std::cout<<std::endl;
+}
+
+void insertAt(int position,int data){
+    Node *temp = head;
+    
+    if(temp == NULL){ //if Linked list is Empty
+        insertNode(data);
+        std::cout<<"Linked List was empty, new element is added at first position."<<std::endl;
+    }else{
+        Node * node = new Node; //creating new node
+        node->data = data; //inserting data
+        
+        for(int i=1;i<position-1;i++){
+            temp = temp->next;
+        } //now we are pointing to one position one previous.
+        
+        node->next = temp->next; //new node will point to node to whom it will replace.
+        temp->next = node; //previous node will point to new node.
+        //hopefully it will work.
+        
+    }
+    
+    std::cout<<"New Linked List after inserting New node at "<<position<<" is ";
+    printLinkedList();
+}
+
+
+void deleteAt(int position){
+    Node* temp = head;
+    
+    if (position == 1){
+        head = temp->next; //head will be pointing next element.
+        delete temp; //temp is pointing to first element i.e. what we have to delete.
+    }
+    for(int i=1;i<position-1;i++){
+        temp = temp->next;
+    }
+    Node* removeNode = temp->next;
+    
+    temp->next = removeNode->next;
+    
+    delete removeNode;
+    
+    printLinkedList();
+}
+
 
 int main(int argc, const char * argv[]) {
-    char inputString[20];
+//    char inputString[20];
     //Char Array will print until \0 comes and stops..
     //Any Space,Enter,Tab etc.
     
@@ -78,17 +161,28 @@ int main(int argc, const char * argv[]) {
      Program ended with exit code: 0
      */
     
-    std::cout<<"Enter you name: "<<std::endl;
-    std::cin>>inputString;
+//    std::cout<<"Enter you name: "<<std::endl;
+//    std::cin>>inputString;
+//
+//
+//    std::cout<<"Your name is: "<<inputString<<std::endl;
+//
+//    std::cout<<"Length of your Name is: "<<getNameLength(inputString)<<std::endl;
+//
+//    reverseString(inputString);
+//
+//    std::cout<<"Checking Palindrome Condition: "<<checkPalindrome(inputString)<<std::endl;
     
-
-    std::cout<<"Your name is: "<<inputString<<std::endl;
-
-    std::cout<<"Length of your Name is: "<<getNameLength(inputString)<<std::endl;
     
-    reverseString(inputString);
+    insertNode(4);
+    insertNode(92);
+    insertNode(18);
+    insertNode(38);
+    printLinkedList();
     
-    std::cout<<"Checking Palindrome Condition: "<<checkPalindrome(inputString)<<std::endl;
+    insertAt(4, 69);
+    
+    deleteAt(1);
     return 0;
 }
 
